00de89b261cfb7b105bc8e9ef9e7f54f1cf71ce2
Yulei Liu <yulei.liu@gmail.com>
2021-09-10 10:21:35 +1000

    First commit

--- main.tf
+++ main.tf
@@ -1,3 +1,113 @@
-terraform {
-  required_version = ">= 1.0"
+//get a list of TFC/E workspaces that has tag 'aws'
+data "tfe_workspace_ids" "aws-apps" {
+  names        = ["*"]
+  tag_names    = ["aws"]
+  organization = "yulei"
+}
+
+//get a list of TFC/E workspaces that has tag 'azure'
+data "tfe_workspace_ids" "azure-apps" {
+  names        = ["*"]
+  tag_names    = ["azure"]
+  organization = "yulei"
+}
+
+
+//Add AWS credentials as enviroment variables
+resource "tfe_variable" "aws_access_key_id" {
+  for_each     = data.tfe_workspace_ids.aws-apps.ids
+  key          = "AWS_ACCESS_KEY_ID"
+  value        = ""
+  category     = "env"
+  workspace_id = each.value
+  description  = "AWS Access Key ID"
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "tfe_variable" "aws_secret_access_key" {
+  for_each     = data.tfe_workspace_ids.aws-apps.ids
+  key          = "AWS_SECRET_ACCESS_KEY"
+  value        = "my_value_name"
+  sensitive    = true
+  category     = "env"
+  workspace_id = each.value
+  description  = "AWS Secret Access Key"
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "tfe_variable" "aws_session_token" {
+  for_each     = data.tfe_workspace_ids.aws-apps.ids
+  key = "AWS_SESSION_TOKEN"
+  sensitive    = true
+  value        = "my_value_name"
+  category     = "env"
+  workspace_id = each.value
+  description  = "AWS Session Token"
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+##Optional AWS_REGION
+
+# resource "tfe_variable" "aws_region" {
+#   for_each     = data.tfe_workspace_ids.aws-apps.ids
+#   key          = "AWS_REGION"
+#   value        = var.aws_default_region
+#   category     = "env"
+#   workspace_id = each.value
+#   description  = "AWS REGION"
+# }
+
+resource "tfe_variable" "azure_subscription_id" {
+  for_each     = data.tfe_workspace_ids.azure-apps.ids
+  key          = "ARM_SUBSCRIPTION_ID"
+  value        = ""
+  category     = "env"
+  workspace_id = each.value
+  description  = "Azure Subscription Id"
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "tfe_variable" "azure_tenant_id" {
+  for_each     = data.tfe_workspace_ids.azure-apps.ids
+  key          = "ARM_TENANT_ID"
+  value        = ""
+  category     = "env"
+  workspace_id = each.value
+  description  = "Azure Tenant Id"
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "tfe_variable" "azure_client_id" {
+  for_each     = data.tfe_workspace_ids.azure-apps.ids
+  key          = "ARM_CLIENT_ID"
+  value        = ""
+  category     = "env"
+  workspace_id = each.value
+  description  = "Azure Client Id"
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "tfe_variable" "azure_client_secret" {
+  for_each     = data.tfe_workspace_ids.azure-apps.ids
+  key          = "ARM_CLIENT_SECRET"
+  value        = ""
+  category     = "env"
+  workspace_id = each.value
+  sensitive = true
+  description  = "Azure Client Secret"
+  lifecycle {
+    ignore_changes = [value]
+  }
 }
